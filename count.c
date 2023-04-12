225138ab193f023fcc15d47eb733d0c02b139bff
HHHguishan <HHHguishan@outlook.com>
2023-04-12 17:10:59 +0800

    .c

---
+++ count.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+printf("please input two num to sum\n");
+int a,b;
+scanf("%d %d",&a,&b);
+printf("sum=%d",a+b);
+printf("hahaha");
+return 0;
+
+
+}
---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+
+	printf("please input two number to sum");
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("sum=%d",a+b);
+	return 0;
+
+}
+	
